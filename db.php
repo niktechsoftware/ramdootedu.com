f533a64c485b9ebea2e1097c149650dced5a26a7
unknown <shraddha1507>
2019-01-26 00:54:49 +0530

    asdfghj

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-  $connect = mysqli_connect('localhost','gfincuct_ramdoo','rahul!123singh');
-  mysqli_select_db('gfincuct_ramdoo');
+  $connect = mysql_connect('localhost','gfincuct_ramdoo','rahul!123singh');
+  mysql_select_db('gfincuct_ramdoo');
 ?>
