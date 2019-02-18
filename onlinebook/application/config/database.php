a0f941971f3f0097531c0d9d8e1fb1f1a05e6151
shraddha1507 <sgupta.chk1507@gmail.com>
2019-02-18 11:05:25 +0530

    add

--- onlinebook/application/config/database.php
+++ onlinebook/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'ramdoot';
+$db['default']['username'] = 'gfincuct_book';
+$db['default']['password'] = 'Rahul!123singh';
+$db['default']['database'] = 'gfincuct_onlinebook';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
