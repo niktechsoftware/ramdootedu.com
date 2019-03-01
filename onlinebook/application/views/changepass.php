c8b025843e558860f3daa216e3b5826214e5d1b0
shraddha1507 <sgupta.chk1507@gmail.com>
2019-03-01 14:55:01 +0530

    add

--- onlinebook/application/views/changepass.php
+++ onlinebook/application/views/changepass.php
@@ -1,12 +1,22 @@
 
-<div class="row">
+<div class="row "><br><br>
 	<div class="col-sm-4"></div>
-<div class="col-sm-4">
+
+<div class="col-sm-4 jumbotron">
 <form class="form-group" method="post" action="<?php echo base_url();?>login/changepassword">
 	<input type="hidden" name="id" value="<?php echo $body->id;?>"><br>
-	New Password:<input type="text" name="newpass" placeholder="Enter New password" class="form-control">
-    Confirm Password:<input type="text" name="confirmpass" placeholder="Confirm Your password" class="form-control"><br>
-	<input type="submit" name="submit" value="Change password">
+	<div class="row">
+	<div class="col-sm-1"></div>
+	<div class="col-sm-2">
+	<label><b>New Password:</b></label></div><div class="col-sm-8"><input type="text" name="newpass" placeholder="Enter New password" class="form-control"></div><div class="col-sm-1"></div>
+     </div> <br>
+     <div class="row">
+     	<div class="col-sm-1"></div>
+    <div class="col-sm-2">
+   <label> <b>Confirm Password:</b></label></div><div class="col-sm-8"><input type="text" name="confirmpass" placeholder="Confirm Your password" class="form-control"></div>
+   <div class="col-sm-1"></div>
+   </div><br><center>
+	<input type="submit" name="submit" class="btn btn-info" value="Change password"></center>
 
 </form>
 </div></div>
